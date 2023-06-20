e3e5f3baa696e859624e3d3769d97523532a5e2f
crossrepublic <71635944+ziminl@users.noreply.github.com>
2023-06-20 10:56:35 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+
+import requests
+import json
+webhookurl = 'webhookurl'
+payload = {
+  'content': 'Hello, Discord Webhook!',
+  'username': 'username',
+  'avatar_url': 'https://en.wikipedia.org/wiki/File:Test-Logo.svg'
+}
+
+# Send the webhook message
+response = requests.post(webhookurl, json=payload)
+
+if response.status_code == 204:
+    print('sent')
+else:
+    print('error:', response.status_code)
+
+
+
