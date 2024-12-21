fd15a24fafecf679e9f6c17fd146426765637785
crossrepublic <71635944+ziminl@users.noreply.github.com>
2024-12-21 13:56:38 +0900

    Create oneline.py

---
+++ oneline.py
@@ -0,0 +1,7 @@
+
+import requests
+import json
+
+webhookurl = 'webhookurl'
+
+requests.post('webhookurl', json={'content': 'text2change', 'username': 'username2change', 'avatar_url': 'https://i.imgur.com/oBPXx0D.png'}) 
