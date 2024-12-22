850cd562922dc7a69ff6289f642e3ad186bbe72a
crossrepublic <71635944+ziminl@users.noreply.github.com>
2024-12-22 11:15:24 +0900

    Update oneline.py

--- oneline.py
+++ oneline.py
@@ -4,4 +4,4 @@ import json
 
 webhookurl = 'webhookurl'
 
-requests.post('webhookurl', json={'content': 'text2change', 'username': 'username2change', 'avatar_url': 'https://i.imgur.com/oBPXx0D.png'}) 
+requests.post(webhookurl, json={'content': 'text2change', 'username': 'username2change', 'avatar_url': 'https://i.imgur.com/oBPXx0D.png'}) 
